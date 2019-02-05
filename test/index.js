6bc48f4c6ad5ba15f25afc7fdb6dcf412bf2f9f2
ihollander <ianholla@gmail.com>
2019-02-05 11:34:17 -0500

    Done.

--- test/index.js
+++ test/index.js
@@ -1,31 +1,30 @@
-const chai = require('chai');
-const chaiSpies = require('chai-spies');
-const five = require('five');
+const chai = require("chai"); // chai 4+ breaks
+const chaiSpies = require("chai-spies");
+const five = require("five");
 
 chai.use(chaiSpies);
 
-const lib = require('../lib');
+const lib = require("../lib");
 
 const expect = chai.expect;
 
-describe('lib', () => {
+describe("lib", () => {
   afterEach(() => {
     console.log.reset && console.log.reset();
   });
 
-  it('logs the result of calling helloFlatiron()', () => {
-    const spy = chai.spy.on(console, 'log');
+  it("logs the result of calling helloFlatiron()", () => {
+    const spy = chai.spy.on(console, "log");
 
-    lib('foo', spy);
+    lib("foo", spy);
 
-    expect(spy).to.have.been.called.with('H-e-l-l-o-,- -f-o-o-.');
+    expect(spy).to.have.been.called.with("H-e-l-l-o-,- -f-o-o-.");
   });
 
+  it("logs some calls to five()", () => {
+    const spy = chai.spy.on(console, "log");
 
-  it('logs some calls to five()', () => {
-    const spy = chai.spy.on(console, 'log');
-
-    lib('foo', spy);
+    lib("foo", spy);
 
     expect(spy).to.have.been.called.with(
       `High ${five.loud()}! ${five.upHigh()}`
